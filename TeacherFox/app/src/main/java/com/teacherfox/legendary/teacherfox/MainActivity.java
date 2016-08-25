c654c08e61b5083a2f21cab2efa35d444a4c3f8f
LegendaryCorp <danielpmacedo2500@hotmail.com>
2016-08-25 18:43:43 -0300

    Troca de telas

--- TeacherFox/app/src/main/java/com/teacherfox/legendary/teacherfox/MainActivity.java
+++ TeacherFox/app/src/main/java/com/teacherfox/legendary/teacherfox/MainActivity.java
@@ -1,7 +1,10 @@
 package com.teacherfox.legendary.teacherfox;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void msgteste(View view){
+        setContentView(R.layout.mainmenu);
     }
 }
